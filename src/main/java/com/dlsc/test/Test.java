d012f7a8b7f9131b1cf53db3a15ff483d0169eed
dlemmermann <dlemmermann@gmail.com>
2017-11-07 16:33:36 +0100

    Initial commit

---
+++ src/main/java/com/dlsc/test/Test.java
@@ -0,0 +1,11 @@
+package com.dlsc.test;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
+
+public class Test {
+
+    public static void main(String[] args) {
+        FontAwesomeIconFactory.get().createIcon(FontAwesomeIcon.ADDRESS_BOOK);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module test {
+
+    requires javafx.base;
+    requires javafx.controls;
+
+    requires de.jensd.fx.glyphs.commons;
+    requires de.jensd.fx.glyphs.fontawesome;
+}
